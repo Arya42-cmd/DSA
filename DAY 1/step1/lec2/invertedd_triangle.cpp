be67139da407915a51788321f545e102ff17d32c
Arya <aryamuthumanickam@gmail.com>
2026-07-10 01:12:56 +0530

    modified

---
+++ DAY 1/step1/lec2/invertedd_triangle.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void rectangle_pattern(int n){
+    for(int i = n; i>=1; i--){
+        for(int j = 1; j<= i; j++){
+             cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }   
+}
+int main() {
+    int n;
+    cin>>n;
+rectangle_pattern(n);
+      return 0;  
+    }
+
+    
