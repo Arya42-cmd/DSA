f9ee83cfa20e0833c6ecd027472d9f83daa29c84
Arya <aryamuthumanickam@gmail.com>
2026-07-12 23:51:36 +0530

    trial

--- DAY 1/step1/lec2/binary_triangle.cpp
+++ DAY 1/step1/lec2/binary_triangle.cpp
@@ -1,13 +1,24 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
-void binary_traingle(int n){
-    for(int i = 1;i<)
-}
+
 int main() {
-    int n;
-    cin>>n;
-    binary_triangle(n);
+    int n = 5;
+
+    for (int i = 1; i <= n; i++) {
+        int val;
+
+        if (i % 2 == 1)
+            val = 1;      // Odd row starts with 1
+        else
+            val = 0;      // Even row starts with 0
+
+        for (int j = 1; j <= i; j++) {
+            cout << val << " ";
+            val = 1 - val;   // Toggle between 0 and 1
+        }
+
+        cout << endl;
+    }
 
     return 0;
 }
